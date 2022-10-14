85ea29f604eed7d63f970b73f68a402ebb2c4e82
Sarah <sarah.pederzani@gmx.de>
2022-10-14 15:49:19 +0100

    Z1 calibration - start with plot of calibration data

--- 04_estimate_d18Odw/estimate_d18Odw_from_d18Ophos.R
+++ 04_estimate_d18Odw/estimate_d18Odw_from_d18Ophos.R
@@ -2,8 +2,22 @@
 # this script will convert bioapatite phosphate d18O values to estimates of d18O of drinking water
 # equivalent to the Z1 step in Pryor et al. 2014
 
+#### load libraries ####
+library(ggplot2)
+library(dplyr)
+
 # read the d18Odw-d18Ophos calibration data
 
 dwcal <- read.csv("02_calibration_data/Pryor_Equus_d18Odw_d18Ophos_calibration_data.csv")
 
 head(dwcal)
+
+#### initial plot ####
+
+ggplot(dwcal, aes(x = d18Odw, y = d18Ophos))+
+  theme_bw()+
+  geom_point(size = 3)+
+  xlim(-20, 5)+
+  ylim(0, 30)
+
+
