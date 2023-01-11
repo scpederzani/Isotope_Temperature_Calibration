d9bf2d843918af0e201d8fee3b8a8ac4cc6c1078
Sarah Pederzani <sarah.pederzani@gmx.de>
2023-01-11 12:41:51 +0000

    Z2 - start calculating interim terms for OLS errors

--- 05_estimate_temperature/estimate_Tair_from_d18Odw.R
+++ 05_estimate_temperature/estimate_Tair_from_d18Odw.R
@@ -18,3 +18,19 @@ ggplot2::ggplot(tcal, ggplot2::aes(x = Tair, y = d18Oprecip))+
   ggplot2::geom_point(size = 3)+
   ggplot2::xlim(5, 15)+
   ggplot2::ylim(-12, -5)
+
+#### calculate first interim terms for OLS fit uncertainty ####
+
+tcal_errors <- tcal |>
+  dplyr::mutate(ttbar = Tair - mean(tcal$Tair),
+                ttbar2 = ttbar^2, 
+                xxbar = d18Oprecip - mean(tcal$d18Oprecip), # for now following Pryor variable names
+                xxbar2 = xxbar^2, # where x is d18Owater throughout, but might change later
+                xxbar_by_ttbar = xxbar*ttbar) # because it means x is now on the y-axis here
+
+
+
+
+
+
+
