b3b5b8220df49fa731980402ca4589d38683855f
Sarah Pederzani <sarah.pederzani@gmx.de>
2023-01-11 12:31:21 +0000

    Z2 - load and plot calibration data

--- 05_estimate_temperature/estimate_Tair_from_d18Odw.R
+++ 05_estimate_temperature/estimate_Tair_from_d18Odw.R
@@ -4,3 +4,17 @@
 #### load libraries ####
 library(ggplot2)
 library(dplyr)
+
+# read the Tair-d18Oprecip calibration data
+
+tcal <- read.csv("02_calibration_data/Pryor_Europe_Tair_d18Oprecip_calibration_data.csv")
+
+head(tcal)
+
+#### initial plot ####
+
+ggplot2::ggplot(tcal, ggplot2::aes(x = Tair, y = d18Oprecip))+
+  ggplot2::theme_bw()+
+  ggplot2::geom_point(size = 3)+
+  ggplot2::xlim(5, 15)+
+  ggplot2::ylim(-12, -5)
