a780c1564f118f434cf339a907ceb4856e65a060
Sarah Pederzani <sarah.pederzani@gmx.de>
2023-01-11 12:24:43 +0000

    Z2 - start Tair estimation script

---
+++ 05_estimate_temperature/estimate_Tair_from_d18Odw.R
@@ -0,0 +1,6 @@
+# this script will convert water d18O values to estimates of air temperature
+# equivalent to the Z2 step in Pryor et al. 2014
+
+#### load libraries ####
+library(ggplot2)
+library(dplyr)
